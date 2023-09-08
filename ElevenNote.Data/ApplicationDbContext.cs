c9aa2f9835d1007ff863e91d5b2f8657f1637398
David Ervin <daervin777@gmail.com>
2023-09-07 20:11:06 -0400

    Updated DbContext and added UserEntity

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using ElevenNote.Data.Entites;
 
 namespace ElevenNote.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) { }
+        : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
     }
 }
