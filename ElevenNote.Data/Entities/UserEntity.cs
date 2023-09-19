a8106bd5b99b76555dfd969aed04b8fd54c4c222
David Ervin <daervin777@gmail.com>
2023-09-19 19:09:49 -0400

    Setup UserController file

--- ElevenNote.Data/Entities/UserEntity.cs
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ElevenNote.Data.Entities
--- ElevenNote.WebAPI/Program.cs
+++ ElevenNote.WebAPI/Program.cs
@@ -11,7 +11,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 // Add User Service/Interface for Dependency Injection here
-services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
