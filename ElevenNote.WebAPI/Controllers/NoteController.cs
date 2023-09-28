76d9671faed9ef3b6cf8cca71f84e35179c47a11
David Ervin <daervin777@gmail.com>
2023-09-28 17:19:36 -0400

    Setup NoteController to access NoteService

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ElevenNote.Services.Note;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+    }
+}
