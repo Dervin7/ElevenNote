f7ee1b354a8113b856636eec3f058a9933f93774
David Ervin <daervin777@gmail.com>
2023-09-28 17:39:49 -0400

    Added method for getting all notes to INoteService and NoteController

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -1,22 +1,43 @@
+using System.Reflection.Metadata;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using ElevenNote.Models.Note;
+using ElevenNote.Data;
 
 namespace ElevenNote.Services.Note
 {
     public class NoteService : INoteService
     {
         private readonly int _userId;
-        public NoteService(IHttpContextAccessor httpContextAccessor)
+        private readonly ApplicationDbContext _dbContext;
+        public NoteService(IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
         {
             var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
             var value = userClaims.FindFirst("Id")?.Value;
             var validId = int.TryParse(value, out _userId);
             if (!validId)
                 throw new Exception("Attempted to build NoteService without User Id claim.");
+
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<NoteListItem>> GetAllNotesAsync()
+        {
+            var notes = await _dbContext.notes
+            .Where(entity => entity.OwnerId == _userId)
+            .Select(entity => new NoteListItem
+            {
+                Id = entity.Id,
+                Title = entity.Title,
+                CreatedUtc = entity.CreatedUtc
+            })
+            .ToListAsync();
+
+            return notes;
         }
     }
 }
--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -20,5 +20,13 @@ namespace ElevenNote.WebAPI.Controllers
         {
             _noteService = noteService;
         }
+
+        // GET api/Note
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            var notes = await _noteService.GetAllNotesAsync();
+            return Ok(notes);
+        }
     }
 }
